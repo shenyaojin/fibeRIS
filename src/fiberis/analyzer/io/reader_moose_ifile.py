980221dcec66f0b804c26025ad3063fb3917b51c
shenyaojin <shenyaojin@mines.edu>
2025-05-16 11:27:34 -0600

    Update MOOSE code.

---
+++ src/fiberis/analyzer/io/reader_moose_ifile.py
@@ -0,0 +1,23 @@
+# This file is part of MOOSE I/O module.
+# Purpose: Read MOOSE .i input files and store them in fibeRIS data format.
+# Shenyao Jin, 05/13/2025
+
+import numpy as np
+from fiberis.analyzer.io import core
+
+class MOOSEIFileReader(core.DataIO):
+
+    def __init__(self, fname):
+        """
+        :param fname: Name of the MOOSE input file. Can be the single file name, or the .i file (full name).
+        Initialize the MOOSE I/O
+        """
+        self.fname = fname
+
+    def read(self, **kwargs):
+
+        return
+
+    def write(self, filename, *args):
+
+        return
