9d646acbdbc8f365efcfa88b8de7624464b78357
shenyaojin <shenyaojin@mines.edu>
2025-06-22 22:47:15 -0600

    Refractor data 3d.

--- src/fiberis/analyzer/Data3D/core3D.py
+++
@@ -1,4 +0,0 @@
-# Core 3D for 3D data processing.
-# The 3D data would have xaxis, yaxis,taxis, and data. It's a time discretized 3D data.
-# for those don't have time axis, but have z axis, use geometry3D instead.
-# TBD in the future.
