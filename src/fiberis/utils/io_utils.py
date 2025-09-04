e817ea00cecbf195e8d35da2294f0833c2c872c1
shenyaojin <shenyaojin@mines.edu>
2025-09-04 12:21:02 -0600

    Update h5 reader.

--- src/fiberis/utils/io_utils.py
+++ src/fiberis/utils/io_utils.py
@@ -1,27 +1,67 @@
 # Toolkit reading files. Might be useful for other projects so I put it here.
 import h5py
-import datetime
 
-# Modified from Jin's OptaSense DAS processing code
+# Dictionary of possible HDF5 keys for DAS data components.
+# The function will iterate through these keys to find the correct ones.
+H5_KEYS = {
+    'data': ['data', 'Acquisition/Raw[0]/RawData'],
+    'daxis': ['depth'],
+    'taxis': ['stamps_unix', 'Acquisition/Raw[0]/RawDataTime'],
+    'start_time': ['stamps']
+}
+
+
+# Modified from Jin's OptaSense DAS processing code.
 def read_h5(filename):
     """
-    Reads a 2D matrix and a 1D datetime series from an HDF5 file and converts the time to Python datetime objects.
+    Reads data, axes, and metadata from an HDF5 file by searching for a set of possible keys.
 
     Parameters:
     filename (str): Path to the HDF5 file.
 
     Returns:
     tuple: A tuple containing the following elements:
-        - 2D numpy array: The sensor data.
-        - 1D list: The timestamps corresponding to the sensor data, converted to Python datetime objects.
+        - data (numpy.ndarray): The 2D sensor data. (None if not found)
+        - daxis (numpy.ndarray): The 1D depth/distance axis. (None if not found)
+        - taxis (numpy.ndarray): The 1D raw timestamps. (None if not found)
+        - start_time: The acquisition start time, type depends on H5 file. (None if not found)
     """
+    data = None
+    daxis = None
+    taxis = None
+    start_time = None
+
     with h5py.File(filename, 'r') as file:
-        # Read the 2D matrix ('RawData') and 1D series ('RawDataTime')
-        # from the HDF5 file, this works for the OptaSense DAS files.
-        raw_data = file['Acquisition/Raw[0]/RawData'][:]
-        raw_data_time = file['Acquisition/Raw[0]/RawDataTime'][:]
+        # --- Read Data (2D matrix) ---
+        for key in H5_KEYS['data']:
+            try:
+                data = file[key][:]
+                break
+            except KeyError:
+                continue
 
-    # Convert 'RawDataTime' to Python datetime objects
-    raw_data_time = [datetime.datetime.fromtimestamp(ts / 1e6) for ts in raw_data_time]
+        # --- Read Depth Axis (daxis) ---
+        for key in H5_KEYS['daxis']:
+            try:
+                daxis = file[key][:]
+                break
+            except KeyError:
+                continue
 
-    return raw_data, raw_data_time
+        # --- Read Time Axis (taxis) ---
+        for key in H5_KEYS['taxis']:
+            try:
+                taxis = file[key][:]
+                break
+            except KeyError:
+                continue
+
+        # --- Read Start Time ---
+        for key in H5_KEYS['start_time']:
+            try:
+                start_time = file[key][0]
+                break
+            except KeyError:
+                continue
+
+    return data, daxis, taxis, start_time
