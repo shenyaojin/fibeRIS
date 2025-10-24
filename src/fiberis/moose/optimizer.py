bb65e9433052bf20ac9667161e8c1a5b17bbf0b0
shenyaojin <shenyaojin@mines.edu>
2025-10-24 11:12:12 -0600

    Update optimizer. This is the last update before I finish the model editor.

--- src/fiberis/moose/optimizer.py
+++ src/fiberis/moose/optimizer.py
@@ -1,6 +1,6 @@
 # src/moose/optimizer.py
 # Description: This file contains the implementation of optimization algorithms for inverse problems.
-# Dependencies: MOOSE stochastic tools. You will have it once you compile MOOSE.
+# Dependencies: MOOSE. Optimizer will not use MOOSE provided optimization module, but will use MOOSE to run simulations.
 # Shenyao Jin, 09/24/2025
 
 from typing import List, Dict, Any, Union, Tuple, Optional
